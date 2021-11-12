71ebf0e923b6a6384869b506781666d28718908f
thakyZ <thakyz@outlook.com>
2021-11-12 13:53:05 -0800

    Cleaned some code

--- src/EDQuickLauncher/Settings/Parsers/DirectoryInfoParser.cs
+++ src/EDQuickLauncher/Settings/Parsers/DirectoryInfoParser.cs
@@ -1,33 +1,28 @@
 ﻿/* XIVQuickLauncher - Modified Code
  * Copyright (C) 2021  goatcorp
  */
+
 using Config.Net;
 using System;
 using System.Collections.Generic;
 using System.IO;
 
 namespace EDQuickLauncher.Settings.Parsers {
+
   internal class DirectoryInfoParser : ITypeParser {
     public IEnumerable<Type> SupportedTypes => new[] { typeof(DirectoryInfo) };
 
-    public string ToRawString(object value)
-    {
-      if (value is DirectoryInfo di)
-        return di.FullName;
-
-      return null;
+    public string ToRawString(object value) {
+      return value is DirectoryInfo di ? di.FullName : null;
     }
 
-    public bool TryParse(string value, Type t, out object result)
-    {
-      if (value == null)
-      {
+    public bool TryParse(string value, Type t, out object result) {
+      if (value == null) {
         result = null;
         return false;
       }
 
-      if (t == typeof(DirectoryInfo))
-      {
+      if (t == typeof(DirectoryInfo)) {
         var di = new DirectoryInfo(value);
         result = di;
         return true;
